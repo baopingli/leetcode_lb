8ced567b4c6d5696b3c8590e98a661636bddc86e
baopingli <1123530865@qq.com>
2019-02-12 17:17:06 +0800

    292 zz nim game

---
+++ 292.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool canWinNim(int n) {
+	    return n%4!=0;
+    }
+};
+int main()
+{
+	cout<<"hello world!"<<endl;
+	return 0;
+}
