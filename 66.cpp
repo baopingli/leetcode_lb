610403395fe96b7edc234ecbfe86d4d727b10667
baopingli <1123530865@qq.com>
2019-03-05 17:49:44 +0800

    66

---
+++ 66.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        int i=0;
+
+    }
+};
+int main()
+{
+
+    return 0;
+}
