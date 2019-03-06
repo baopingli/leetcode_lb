1cef38848be90da310a4091f646a06a267bd85c9
baopingli <1123530865@qq.com>
2019-03-06 15:46:19 +0800

    849

---
+++ 849.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+
+    }
+};
+int main()
+{
+
+    return 0;
+}
