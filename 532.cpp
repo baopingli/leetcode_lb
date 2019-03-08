dfb0750e3df1ab7971d3d5c5245ce7d3cdb2fa1e
baopingli <1123530865@qq.com>
2019-03-08 17:53:45 +0800

    532 unordered map

---
+++ 532.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int res=0,n=nums.size();
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++)
+        {
+            ++m[nums[i]];
+        }
+        for(auto a:m)
+        {
+            if(k==0&&a.second>1)
+                res++;
+            if(k>0&&m.count(a.first+k))
+                res++;
+        }
+        return res;
+    }
+};
+int main()
+{
+    return 0;
+}
