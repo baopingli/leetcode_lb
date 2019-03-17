f4753af6dd8cba3d91b5f385cb544b6dba159c5d
baopingli <1123530865@qq.com>
2019-03-17 09:53:48 +0800

    893

---
+++ 893.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int numSpecialEquivGroups(vector<string>& A) {
+
+    }
+};
+int main()
+{
+
+    return 0;
+}
