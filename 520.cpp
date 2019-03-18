8706794c92f8619fef65e374af50a6a47bcbda9e
baopingli <1123530865@qq.com>
2019-03-18 13:57:28 +0800

    520

---
+++ 520.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+
+
+    }
+};
+int main()
+{
+    cout<<'a'-'A'<<endl;
+
+    return 0;
+}
