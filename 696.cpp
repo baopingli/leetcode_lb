df3f60fcbebbb4050348e18ecc03d7b3d250aba9
baopingli <1123530865@qq.com>
2019-04-01 21:39:12 +0800

    696 and 136

---
+++ 696.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int res=0;
+        for(int i=0;i<s.length();i++)
+        {
+
+        }
+    }
+};
+int main(){
+return 0;
+}
